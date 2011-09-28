b7d8272d2b4dff444a359262d9301cbac8500323
Bruned <Bruned@130.66.206.217>
2011-09-28 10:24:59 +0200

    Création d'une classe bonjour
    Signed-off-by: Bruned <Bruned@130.66.206.217>

---
+++ src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		Point p1= new Point(0,0); 
+        Point p2= new Point(1,2);
+        System.out.println(p1.toString()+p2.toString());
+        		
+	}
+	
+}
